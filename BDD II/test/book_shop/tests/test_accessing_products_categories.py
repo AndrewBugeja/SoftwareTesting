799f3752fd32d24c0ecde94ff6e5e7bf74ada1f6
AndrewBugeja <104757391+AndrewBugeja@users.noreply.github.com>
2024-02-15 23:04:30 +0100

    Update test_accessing_products_categories.py

--- BDD II/test/book_shop/tests/test_accessing_products_categories.py
+++ BDD II/test/book_shop/tests/test_accessing_products_categories.py
@@ -9,7 +9,7 @@ class TestProductCategories:
     @pytest.mark.parametrize("category_name,num_products", [
         ("Signed", 1),
         ("Bestsellers", 2),
-        ("Bookseller", 3),
+        ("Bookseller-picks", 3),
         ("Childrens", 5),
         ("Poetry", 3)
     ])
@@ -18,8 +18,8 @@ class TestProductCategories:
         categories_page = CategoriesPage(driver)
         categories_page.load_website()
 
-        categories_page.click_on_a_category("Signed")
-        assert categories_page.verify_category_is_loaded("Signed"), "Category not loaded"
-        assert categories_page.verify_categories_has_at_least_n_number_of_products("2"), "n Products not found"
+        categories_page.click_on_a_category(category_name)
+        assert categories_page.verify_category_is_loaded(category_name), "Category not loaded"
+        assert categories_page.verify_categories_has_at_least_n_number_of_products(num_products), "n Products not found"
         categories_page.click_on_the_first_product()
         assert categories_page.verify_we_are_on_details_page_of_the_product(), "product details page is not loaded"
